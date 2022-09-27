73aa96407a93db53938896fb67f89c376c64ac66
Hunter <hunter.lane@student.nmt.edu>
2022-09-26 20:07:40 -0600

    Made some changes

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ all: Voidcat run clean
 
 Voidcat: program.cpp 
 	g++ program.cpp -o Voidcat
+	./Voidcat
+	rm Voidcat
 
 run: Voidcat
 	./Voidcat
--- program.cpp
+++ program.cpp
@@ -14,9 +14,14 @@
 
 using namespace std;
 
-
-
 int main()
 {
-    
+    cout << "  /\\  /\\" << endl;
+    cout << " / _  _ \\" << endl;
+    cout << "/  O  O  \\" << endl;
+    cout << "\\   /\\   /" << endl;
+    for (int i = 0; i < 20; i++)
+        cout << "|   ||   |" << endl;
+    cout << " W      W" << endl;
+    return 0;
 }
